dc25919db4a21c5d3c686fa996bbdd7dbfa73bbb
SUP3RS0N1C <sputnik1957@sookmyung.ac.kr>
2019-09-19 15:34:08 +0900

    실습 2

--- main.c
+++ main.c
@@ -5,7 +5,17 @@
 
 int main(int argc, char *argv[]) {
 	
-	printf("Hello Sookmyung EE!\n");
+	int x;
+	char x_1;
+	short x_2;
+	float x_3;
+	double x_4;
 	
+	printf("bytes of int : %d\n", sizeof(x) );
+	printf("bytes of char : %d\n", sizeof(x_1) );
+	printf("bytes of short : %d\n", sizeof(x_2) );
+	printf("bytes of float : %d\n", sizeof(x_3) );
+	printf("bytes of double : %d\n", sizeof(x_4) );
+		
 	return 0;
 } 
